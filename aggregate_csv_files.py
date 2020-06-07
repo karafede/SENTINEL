09c5257e7c78b99d950f04c048c32bd37d5e8d61
Federico Karagulian <karafede@hotmail.com>
2020-06-07 17:18:41 +0200

    Add files via upload

--- aggregate_csv_files.py
+++ aggregate_csv_files.py
@@ -4,65 +4,186 @@ import glob
 import pandas as pd
 import db_connect
 import sqlalchemy as sal
-os.chdir('C:/python/projects/giraffe/viasat_data/CSVs')
+import csv
+import psycopg2
+os.chdir('D:/ViaSat/Salerno')
 cwd = os.getcwd()
 
-
 # connect to new DB to be populated with Viasat data after route-check
-conn_HAIG = db_connect.connect_HAIG_Viasat_CT()
+conn_HAIG = db_connect.connect_HAIG_Viasat_SA()
 cur_HAIG = conn_HAIG.cursor()
 
+
+## create extension postgis on the database HAIG_Viasat_SA  (only one time)
+
+# cur_HAIG.execute("""
+# CREATE EXTENSION postgis
+# """)
+# cur_HAIG.execute("""
+# CREATE EXTENSION postgis_topology
+# """)
+# conn_HAIG.commit()
+
+
+# Create an SQL connection engine to the output DB
+engine = sal.create_engine('postgresql://postgres:superuser@192.168.132.18:5432/HAIG_Viasat_SA')
+connection = engine.connect()
+
 # erase existing table
-cur_HAIG.execute("DROP TABLE IF EXISTS prova_viasat_files_csv CASCADE")
-conn_HAIG.commit()
+# cur_HAIG.execute("DROP TABLE IF EXISTS obu CASCADE")
+# conn_HAIG.commit()
 
 # Function to generate WKB hex
 def wkb_hexer(line):
     return line.wkb_hex
 
-## creat empty table to host all VIASAT data
-# dateTime timestamp NOT NULL,
-# cur_HAIG.execute("""
-#      CREATE  TABLE "prova_viasat_files_csv"(
-#      idRequest integer,
-#      deviceId integer  ,
-#      dateTime timestamp ,
-#      latitude numeric ,
-#      longitude numeric ,
-#      speedKmh integer ,
-#      heading integer ,
-#      accuracyDop integer ,
-#      EngnineStatus integer ,
-#      Type integer ,
-#      Odometer integer)
-#      """)
-#
-# conn_HAIG.commit()
+###########################
+## create OBU table #######
+###########################
+
+static_csv = "VST_ENEA_SA_DATISTATICI.csv"
+static_data = pd.read_csv(static_csv, delimiter=';', encoding='latin-1')
+static_data.columns = ['idterm', 'devicetype', 'idvehcategory', 'brand', 'anno',
+                       'portata', 'gender', 'age']
+static_data['anno'] = static_data['anno'].astype('Int64')
+static_data['portata'] = static_data['portata'].astype('Int64')
+len(static_data)
+
+#### create "OBU" table in the DB HAIG_Viasat_SA #####
+## insert static_data into the DB HAIG_Viasat_SA
+static_data.to_sql("obu", con=connection, schema="public", index=False)
 
 
-# Create an SQL connection engine to the output DB
-engine = sal.create_engine('postgresql://postgres:vaxcrio1@localhost:5432/HAIG_Viasat_CT')
+##################################################
+########## VIASAT dataraw ########################
+##################################################
 
 # match pattern of .csv files
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+viasat_filenames = ['VST_ENEA_SA_FCD_2017.csv',    # 83206797 lines
+                    'VST_ENEA_SA_FCD_2019.csv',    # 106473285 lines
+                    'VST_ENEA_SA_FCD_2019_2.csv']  # 79426790 lines
 
-# Combine all files in the list and export as unique CSV
-#combine all files in the list
-# combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-#export to csv
-# combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+## erase existing table
+# cur_HAIG.execute("DROP TABLE IF EXISTS dataraw CASCADE")
+# conn_HAIG.commit()
+
+'''
+# dateTime timestamp NOT NULL,
+cur_HAIG.execute("""
+     CREATE  TABLE dataraw(
+     idrequest bigint,
+     idterm bigint  ,
+     timedate timestamp ,
+     latitude numeric ,
+     longitude numeric ,
+     speed integer ,
+     direction integer ,
+     grade integer ,
+     panel integer ,
+     event integer ,
+     vehtype integer ,
+     progressive bigint,
+     millisec integer,
+     timedate_gps timestamp,
+     distance bigint)
+     """)
+
+conn_HAIG.commit()
+
+sep = ","
+# populate table from the .csv data
+for csv_file in viasat_filenames:
+    print(csv_file)
+    # csv_file = viasat_filenames[0]
+    with open(csv_file, 'r') as f:
+        viasat_data = csv.reader(f, delimiter = ';')
+        print(viasat_data)
+        next(viasat_data)
+        for col in viasat_data:
+            print(col)
+            # print(len(col))
+            if len(col) == 15:
+                print(col[1])
+                idrequest=col[0]
+                idterm=col[1]
+                timedate=col[2]
+                latitude=col[3]
+                longitude=col[4]
+                speed=col[5]
+                direction=col[6]
+                grade=col[7]
+                panel=col[8]
+                event = col[9]
+                vehtype=col[10]
+                progressive=col[11]
+                millisec = col[12]
+                timedate_gps = col[13]
+                distance = col[14]
+                input = "(" + str(idrequest) + sep\
+                        + str(idterm) + sep\
+                        + "'" + str(timedate) + "'" + sep\
+                        + str(latitude) + sep\
+                        + str(longitude) + sep\
+                        + str(speed) + sep\
+                        + str(direction) + sep\
+                        + str(grade) + sep\
+                        + str(panel) + sep \
+                        + str(event) + sep \
+                        + str(vehtype) + sep \
+                        + str(progressive) + sep \
+                        + str(millisec) + sep \
+                        + "'" + str(timedate_gps) + "'" + sep \
+                        + str(distance) + ")"
+                try:
+                    cur_HAIG.execute("INSERT INTO dataraw (idrequest, idterm, timedate, latitude, longitude, speed, "
+                                "direction, grade, panel, event, vehtype, progressive, "
+                                "millisec, timedate_gps, distance)" + " VALUES " +input + "")
+                except psycopg2.errors.NumericValueOutOfRange:
+                    print("skip VALUES")
+
+    conn_HAIG.commit()
+'''
+
+## loop over all the .csv file with the raw VIASAT data
+for csv_file in viasat_filenames:
+# csv_file = viasat_filenames[0]
+    file = open(csv_file)
+    reader = csv.reader(file)
+    ## get length of the csv file
+    lines = len(list(reader))
+    print(lines)
+
+    slice = 100000  # slice of data to be insert into the DB during the loop
+    ## calculate the neccessary number of iteration to carry out in order to upload all data into the DB
+    iter = int(round(lines/slice, ndigits=0)) +1
+    for i in range(0, iter):
+        print(i)
+        print(i, csv_file)
+        # csv_file = viasat_filenames[0]
+        df = pd.read_csv(csv_file, header=None ,delimiter=';', skiprows=i*slice ,nrows=slice)
+        ## define colum names
+        df.columns = ['idrequest', 'idterm', 'timedate', 'latitude', 'longitude',
+                      'speed', 'direction', 'grade', 'panel', 'event', 'vehtype',
+                      'progressive', 'millisec', 'timedate_gps', 'distance']
+        df['id'] = pd.Series(range(i * slice, i * slice + slice))
+        df['timedate'] = df['timedate'].astype('datetime64[ns]')
+        df['timedate_gps'] = df['timedate_gps'].astype('datetime64[ns]')
+        ## upload into the DB
+        df.to_sql("dataraw", con=connection, schema="public",
+                                       if_exists='append', index = False)
 
 
-connection = engine.connect()
-for idx , csv_file in enumerate(all_filenames):
-    df = pd.read_csv(csv_file, header=None)
-    df.columns = ['idRequest', 'deviceId', 'dateTime', 'latitude', 'longitude', 'speedKmh',
-                        'heading', 'accuracyDop', 'EngnineStatus', 'Type', 'Odometer']
-    print(df)
-    df.to_sql("prova_viasat_files_csv", con=connection, schema="public",
-                                   if_exists='append', index = False)
 
+## add geometry WGS84 4286 (Catania, Italy)
+cur_HAIG.execute("""
+alter table dataraw add column geom geometry(POINT,4326)
+""")
+
+cur_HAIG.execute("""
+update dataraw set geom = st_setsrid(st_point(longitude,Latitude),4326)
+""")
+
+conn_HAIG.commit()
 
 
 ## create a consecutive ID for each row
@@ -71,11 +192,15 @@ alter table "prova_viasat_files_csv" add id serial
      """)
 conn_HAIG.commit()
 
+
 ## drop one column
 cur_HAIG.execute("""
 ALTER TABLE "prova_viasat_files_csv" DROP "idRequest"
      """)
 conn_HAIG.commit()
+
+conn_HAIG.close()
+cur_HAIG.close()
 
 
 ## check the "id"
@@ -84,6 +209,7 @@ AAA = pd.read_sql_query('''
                     FROM public.prova_viasat_files_csv''', conn_HAIG)
 
 
+'''
 ### rename the table in order to create a new one with columns in a different order
 cur_HAIG.execute("""
 ALTER TABLE "prova_viasat_files_csv" rename to "prova_viasat_files_csv_old"
@@ -132,3 +258,5 @@ conn_HAIG.commit()
 
 conn_HAIG.close()
 cur_HAIG.close()
+
+'''
