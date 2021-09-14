30650d28c489d1d7bcc3f01dcc2aca4e712b866a
Federico Karagulian <karafede@hotmail.com>
2021-09-14 15:14:56 +0200

    Add files via upload

--- route_SALERNO_FK.py
+++ route_SALERNO_FK.py
@@ -119,8 +119,8 @@ with open("idterms.txt", "w") as file:
 """
 
 # ## reload all 'idterms' as list
-# with open("D:/ENEA_CAS_WORK/SENTINEL/viasat_data/idterms.txt", "r") as file:
-#   idterms = eval(file.readline())
+with open("D:/ENEA_CAS_WORK/SENTINEL/viasat_data/idterms.txt", "r") as file:
+   idterms = eval(file.readline())
 
 #with open("D:/ENEA_CAS_WORK/SENTINEL/viasat_data/idterms_new.txt", "r") as file:
 #   idterms = eval(file.readline())
@@ -161,8 +161,8 @@ def func(arg):
             ### zip the coordinates into a point object and convert to a GeoData Frame ####
             if len(data) > 1:
                 ## find outliers
-                q = data["progressive"].quantile(0.99)
-                data = data[data["progressive"] < q]
+                # q = data["progressive"].quantile(0.99)
+                # data = data[data["progressive"] <= q]
                 if len(data) > 1:
                     geometry = [Point(xy) for xy in zip(data.longitude, data.latitude)]
                     df = GeoDataFrame(data, geometry=geometry)
@@ -208,7 +208,7 @@ def func(arg):
                     ### find distance between coordinates of two consecutive TRIPS in METERS!!!
                     deviation_pos = great_circle_track_node(lon_end, lat_end, lon_start, lat_start)
                     ### build the final dataframe ("route" table)
-                    if tripdistance_m > 0:
+                    if tripdistance_m > 0 and triptime_s <= 25200 and breaktime_s > 60 and triptime_s > 0:  ## <------
                         df_ROUTE = pd.DataFrame({'idtrajectory': [idtrajectory],
                                                  'idterm': [idterm],
                                                  'idtrace_o': [idtrace_o],
